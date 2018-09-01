ea9409576680f33159c80bef07dff6883fc6ebf5
Nicolas Dumazet <nicdumz.commits@gmail.com>
2018-09-01 09:55:38 +0200

    s/GridTableBase/GridStringTable
    It seems that GridTableBase sometimes is called PyGridTableBase, with
    GridTableBase being abstract (__init__ throws). GridStringTable seems to
    usually be there across versions and does what we need.

--- src/csv2ofx/csvutils.py
+++ src/csv2ofx/csvutils.py
@@ -3,16 +3,16 @@
 from datetime import datetime
 import csv
 
-from wx.grid import GridTableBase
+from wx.grid import GridStringTable
 
 
-class SimpleCSVGrid(GridTableBase):
+class SimpleCSVGrid(GridStringTable):
     """
         A very basic instance that allows the csv contents to be used
         in a wx.Grid
     """
     def __init__(self,csv_path,delimiter=',',skip_last=0):
-        GridTableBase.__init__(self)
+        GridStringTable.__init__(self)
           # delimiter, quote could come from config file perhaps
         csv_reader = csv.reader(open(csv_path,'r'),delimiter=delimiter,quotechar='"')
         self.grid_contents = [row for row in csv_reader if len(row)>0]
